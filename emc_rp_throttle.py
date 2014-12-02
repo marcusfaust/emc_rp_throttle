5857650868eaf7547c76735ba8087d33c503b7c6
marcusfaust <marcus.faust1@gmail.com>
2014-12-02 10:08:22 -0800

    Added ip address validation with script termination.

--- emc_rp_throttle.py
+++ emc_rp_throttle.py
@@ -5,6 +5,7 @@ __author__ = 'marcusfaust'
 import requests
 from requests.auth import HTTPBasicAuth
 import sys, argparse
+import socket
 
 BASEURL =''
 AUTH = HTTPBasicAuth('admin', 'admin')
@@ -30,14 +31,29 @@ def outputThrottles(bandwidths):
     print '\n{0:20} : {1:10f} Mbps'.format('TOTAL:', total)
 
 
+def isIP_v2(address):
+    try:
+        socket.inet_aton(address)
+        ip = True
+    except socket.error:
+        ip = False
+
+    return ip
+
+
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Script to display all Consistency Group Bandwidth Limits")
     required_group = parser.add_argument_group('Required Arguments')
     required_group.add_argument('-ip', '--ipaddress', help="Input the ip address of the RPA cluster here", required=True)
     args = parser.parse_args()
+    if isIP_v2(args.ipaddress):
+        BASEURL = 'https://' + args.ipaddress + '/fapi/rest/4_0/settings/groups/all'
 
-    BASEURL = 'https://' + args.ipaddress + '/fapi/rest/4_0/settings/groups/all'
+        bandwidths = getThrottles(BASEURL)
+        outputThrottles(bandwidths)
+    else:
+        sys.exit("Invalid IP Address detected for --ipaddress parameter.  Exiting.")
 
-    bandwidths = getThrottles(BASEURL)
-    outputThrottles(bandwidths)
