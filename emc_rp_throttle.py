d65b381869f2d2143d669616e2a66e9cec17c6d9
marcusfaust <marcus.faust1@gmail.com>
2014-12-02 05:20:39 -0800

    Added argparse and -ip arg. Added outputThrottles function.

--- emc_rp_throttle.py
+++ emc_rp_throttle.py
@@ -1,31 +1,44 @@
+#!/usr/bin/python
+
 __author__ = 'marcusfaust'
 
 import requests
 from requests.auth import HTTPBasicAuth
+import sys, argparse
 
-
-BASEURL = 'https://192.168.151.67/fapi/rest/4_0/settings/groups/all'
+BASEURL =''
 AUTH = HTTPBasicAuth('admin', 'admin')
 
 
-def getThrottles():
+def getThrottles(baseurl):
     bandwidths = {}
     total = 0
-    r = requests.get(BASEURL, verify=False, auth=AUTH)
+    r = requests.get(baseurl, verify=False, auth=AUTH)
     results = r.json()
 
     for group in results:
-        print group['name']
         bandwidths[group['name']] = group['activeLinksSettings'][0]['linkPolicy']['protectionPolicy']['bandwidthLimit']
-        total += group['activeLinksSettings'][0]['linkPolicy']['protectionPolicy']['bandwidthLimit']
 
     bandwidths['total'] = total
     return bandwidths
 
 
+def outputThrottles(bandwidths):
+    total = 0.0
+    print '\n\nCurrent CG Bandwidth Limits:\n'
+    for name, throttle in bandwidths.items():
+        print '{0:20} : {1:10f} Mbps'.format(name, throttle)
+        total += throttle
+    print '\n{0:20} : {1:10f} Mbps'.format('TOTAL:', total)
+
 
 if __name__ == '__main__':
-    bandwidths = getThrottles()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--ipaddress', help="Input the ip address of the RPA cluster here")
+    args = parser.parse_args()
 
-    print "hello"
+    BASEURL = 'https://' + args.ipaddress + '/fapi/rest/4_0/settings/groups/all'
+
+    bandwidths = getThrottles(BASEURL)
+    outputThrottles(bandwidths)
