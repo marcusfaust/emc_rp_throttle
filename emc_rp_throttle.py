c5158b855fc56cdad292f7ad23b6a9712b11f3ac
marcusfaust <marcus.faust1@gmail.com>
2014-12-03 18:17:25 -0800

    Added subparsers set_throttle and get_throttles.  Added set_throttle function skeleton.

--- emc_rp_throttle.py
+++ emc_rp_throttle.py
@@ -4,7 +4,7 @@ __author__ = 'marcusfaust'
 
 import requests
 from requests.auth import HTTPBasicAuth
-import sys, argparse
+import sys, argparse, json
 import socket
 
 BASEURL =''
@@ -52,18 +52,32 @@ def outputThrottles(bandwidths):
         total += throttle
     print '\n{0:20} : {1:10f} Mbps'.format('TOTAL:', total)
 
+
+def setThrottle(cg, throttle, baseurl):
+
+    for uid, name in CG_NAME_MAP.items():
+        if name == cg:
+            url = baseurl + '/settings/groups/' + str(uid) + '/full'
+            r = requests.get(url, verify=False, auth=AUTH)
+            results = r.json()
+            print "done"
+
+
 def outputWanStats(groupstats,groupsettings):
     global CG_NAME_MAP
     wanstats = {}
     total = 0.0
-    print CG_NAME_MAP
     for group in groupstats:
+        wanstats[group['consistencyGroupUID']['id']] = {}
         wanstats[group['consistencyGroupUID']['id']]['name'] = CG_NAME_MAP[group['consistencyGroupUID']['id']]
-    print '\n\nCurrent CG Bandwidth Limits:\n'
-    for name, throttle in bandwidths.items():
-        print '{0:20} : {1:10f} Mbps'.format(name, throttle)
-        total += throttle
-    print '\n{0:20} : {1:10f} Mbps'.format('TOTAL:', total)
+        wanstats[group['consistencyGroupUID']['id']]['init_throughput'] = group['consistencyGroupLinkStatistics'][0]['initStatistics']['initOutgoingThroughput'] / 1000000
+        wanstats[group['consistencyGroupUID']['id']]['init_percent'] = int(group['consistencyGroupLinkStatistics'][0]['initStatistics']['initCompletionPortion'] * 100)
+    print '\n\nCurrent CG WAN Statistics:\n'
+    for group in wanstats:
+        print '{0:20} : {1:10.2f} Mbps : {2:6d} % Init'.format(wanstats[group]['name'], wanstats[group]['init_throughput'], wanstats[group]['init_percent'])
+        total += wanstats[group]['init_throughput']
+
+    print '\n{0:20} : {1:10.2f} Mbps'.format('TOTAL:', total)
 
 
 def isIP_v2(address):
@@ -82,7 +96,13 @@ if __name__ == '__main__':
     actions_group.add_argument('-w', '--wanstats', help="Display WAN CG Statistics", action="store_true")
     required_group = parser.add_argument_group('Required Arguments')
     required_group.add_argument('-ip', '--ipaddress', help="Input the ip address of the RPA cluster here", required=True)
+    subparsers = parser.add_subparsers()
+    set_throttle = subparsers.add_parser('set_throttle', help="Specify this with -cg and -throttle arguments to set a limit on a CG.")
+    set_throttle.add_argument('-cg', '--cg', help="Input the name of the CG here", required=True, dest='cg')
+    set_throttle.add_argument('-throttle', '--throttle', help="Input the Mbps throttle value here.", required=True, dest='throttle')
+    get_throttles = subparsers.add_parser('get_throttles', help="Displays All CG Throttles")
     args = parser.parse_args()
+
     if isIP_v2(args.ipaddress):
         #BASEURL = 'https://' + args.ipaddress + '/fapi/rest/4_0/settings/groups/all'
         BASEURL = 'https://' + args.ipaddress + '/fapi/rest/4_0'
@@ -90,9 +110,15 @@ if __name__ == '__main__':
         groupsettings = getAllGroupSettings(BASEURL)
         CG_NAME_MAP = createGroupNameMap(groupsettings)
         bandwidths = getThrottles(groupsettings)
-        outputThrottles(bandwidths)
+        #outputThrottles(bandwidths)
     else:
         sys.exit("Invalid IP Address detected for --ipaddress parameter.  Exiting.")
 
     if args.wanstats:
         outputWanStats(groupstats, groupsettings)
+
+    if set_throttle:
+        if args.cg in CG_NAME_MAP.values():
+            setThrottle(args.cg, args.throttle, BASEURL)
+    elif get_throttles:
+        outputThrottles(bandwidths)
